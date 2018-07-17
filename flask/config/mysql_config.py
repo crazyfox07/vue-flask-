9c6e7163785878dcff4052587f7dcecb2b5a571b
crazyfox07 <30678687+crazyfox07@users.noreply.github.com>
2018-07-17 09:46:28 +0800

    Update mysql_config.py

--- flask/config/mysql_config.py
+++ flask/config/mysql_config.py
@@ -13,11 +13,11 @@ class Config:
 #开发环境
 class DevelopmentConfig(Config):
     #SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(base_dir,'blog-dev.sqlite')
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:mime@123@99.48.58.95/spider?charset=utf8"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:pwd@host/db?charset=utf8"
 #测试环境
 class TestingConfig(Config):
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:mime@123@99.48.58.95/spider?charset=utf8"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://user:pwd@host/db?charset=utf8"
 
 #生产环境
 class ProductionConfig(Config):
-    SQLALCHEMY_DATABASE_URI =  "mysql+pymysql://root:mime@123@99.48.58.95/spider?charset=utf8"
+    SQLALCHEMY_DATABASE_URI =  "mysql+pymysql://user:pwd@host/db?charset=utf8"
